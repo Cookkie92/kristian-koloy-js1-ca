bcff5ea73b60b9c79c79a215552f6a6c69f02d13
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-10-20 23:24:08 +0200

    plomma

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,32 @@
-fetch("https://free-to-play-games-database.p.rapidapi.com/api/game?id=452", {
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-    "x-rapidapi-key": "a80672ee60msh8679bffac93f402p1f8093jsnd8eb05bce674",
-  },
-})
-  .then((response) => {
-    console.log(response);
+const resaultContainer = document.querySelector(".result");
+resaultContainer.innerHTML = "";
+fetch(
+  "https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc",
+  {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      "x-rapidapi-key": "a80672ee60msh8679bffac93f402p1f8093jsnd8eb05bce674",
+    },
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const games = data;
+    console.log(games);
+    for (let i = 0; i < games.length; i++) {
+      if (i === 25) {
+        break;
+      }
+
+      resaultContainer.innerHTML += `
+      <div class ="result">Name: ${games[i].title}    </div>
+      <div class ="result">Genre: ${games[i].genre}</div>
+      <div class ="result">Platform: ${games[i].platform}</div>
+      <div class ="result">Description: ${games[i].short_description}</div>
+      `;
+    }
   })
   .catch((err) => {
     console.error(err);
   });
-
-// const url =
-//   "https://www.freetogame.com/api/games?platform=browser&category=mmorpg&sort-by=release-date&key=a80672ee60msh8679bffac93f402p1f8093jsnd8eb05bce674";
-
-// const resultContainer = document.querySelector(".result");
-
-// async function getApi() {
-//   const response = await fetch(url);
-//   const result = await response.json();
-//   console.log(result);
-// }
-
-// getApi();
